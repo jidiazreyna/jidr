49412ffb08ecc699428c4059d31e2888d8222b93
jidiazreyna <jidiazreyna@gmail.com>
2025-05-30 13:23:43 -0300

    Add prescripcion package with rendering helper

---
+++ OneDrive/Escritorio/Programas/prescripcion/sobreseimiento_prescripcion.py
@@ -0,0 +1,10 @@
+"""Utilidades para sobreseimientos por prescripción."""
+
+TEMPLATE = """\
+TODO: replace with the actual template text
+"""
+
+
+def render_prescripcion(**campos) -> str:
+    """Devuelve el texto del sobreseimiento formateado."""
+    return TEMPLATE.format(**campos)
